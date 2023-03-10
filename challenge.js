900940b8f18cf79b972aaae77344a48058ae5838
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2023-03-10 08:45:33 +0430

    initail solution

--- challenge.js
+++ challenge.js
@@ -1,5 +1,55 @@
+class Node {
+  constructor() {
+    this.element = 0;
+    this.pointer = null;
+  }
+}
+class Stack {
+  constructor() {
+    this.top = null;
+  }
+  push(number) {
+    let temp = new Node();
+    temp.element = number;
+    temp.pointer = this.top;
+    this.top = temp;
+  }
+
+  pop() {
+    const item = this.top.element;
+    this.top = this.top.pointer;
+    return item;
+  }
+  isEmpty()
+  {
+       return this.top == null;
+  }
+}
 function balancedBrackets(string) {
-  // your code here
+  const stack = new Stack();
+    for(element of [...string]){
+    let result='';
+    if (element == '(' || element == '[' || element == '{') {
+      stack.push(element);
+
+    } else if (element == ')' || element == ']' || element == '}') {
+      let temp = stack.pop();
+      if (temp == '(' && element != ')') {
+        return false;
+      } else if (temp == '[' && element != ']') {
+       return false;
+    
+      } else if (temp == '{' && element != '}') {
+       return false;
+      }
+    }
+  }
+  if(stack.isEmpty()){
+    return true;
+  }else{
+    return false;
+  }
+
 }
 
 console.log(balancedBrackets('(hello)[world]'))
